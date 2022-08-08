7abd0f569fd2a987d5c3458867779b1341b23951
2-Sungho <sungho._.2@icloud.com>
2022-08-08 17:01:42 +0900

    0808

---
+++ 03_control/src/edu/kh/control/condition/ConditionRun.java
@@ -0,0 +1,18 @@
+package edu.kh.control.condition;
+
+// 코드를 실행시키는 부분
+public class ConditionRun {
+
+	public static void main(String[] args) {
+		
+		ConditionExample ex= new ConditionExample();
+		
+//		ex.test1();
+//		ex.test2();
+//		ex.test3();
+	
+//		커서놓고 -> crtl+/ 한줄주석
+//		ex.ex1();
+//		ex.ex2();
+	}
+}
