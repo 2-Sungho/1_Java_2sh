422996627fdd0d526ea13da5383dcb03c1957c72
2-Sungho <sungho._.2@icloud.com>
2022-08-22 16:24:17 +0900

    0822

---
+++ 07_inheritance/src/edu/kh/inheritance/ex/Example.java
@@ -0,0 +1,28 @@
+package edu.kh.inheritance.ex;
+
+import edu.kh.inheritance.model.vo.Student;
+
+public class Example {
+
+	public void ex1() {
+		// 상속 확인
+		// Student 자식 객체 생성
+		Student s1=new Student();
+		
+		// 자식만의 기능/필드(grade,classRoom)
+		s1.setGrade(2); // Student의 메서드
+		s1.setClassRoom(3); // Student의 메서드
+		s1.setName("이성호"); // setName(String name):void-Person(Person의 메서드)
+						  // -> 부모인 Person으로부터 상속받은 메서드라는뜻
+		s1.setAge(15);
+		
+		//int edu.kh.inheritance.model.vo.Student
+		System.out.println(s1.getGrade());
+		System.out.println(s1.getClassRoom());
+		
+		//int edu.kh.inheritance.model.vo.Person
+		System.out.println(s1.getName()); 
+		System.out.println(s1.getAge());
+
+	}
+}
---
+++ 07_inheritance/src/edu/kh/inheritance/model/vo/Person.java
@@ -0,0 +1,34 @@
+package edu.kh.inheritance.model.vo;
+
+public class Person {
+
+	private String name;
+	private int age;
+	
+	// crtl+space bar+enter : 기본 생성자
+	public Person() {}
+	
+	// 매개변수 생성자 : alt+shift+s -> o
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+}
